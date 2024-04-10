693e59f515d116382c4a721ab52beac8cfd2db4d
Hiron Das <hiron9340@gmail.com>
2024-04-11 04:57:40 +0600

    added some logcs and scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
+// function play() {
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+//     const playSection = document.getElementById('play-ground');
+//     playSection.classList.remove('hidden');
+// }
+
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+    // console.log('Your random alphabet is:', alphabet);
+
+    // set randomly generated alphabet in screen
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color of key
+    setBackgroundColorById(alphabet);
+}
+
+
 function play() {
-    console.log('Starting by click play');
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
