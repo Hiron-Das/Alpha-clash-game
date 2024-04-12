622f4fdbf96ae33aa00910ad44839adac878ca70
Hiron Das <hiron9340@gmail.com>
2024-04-12 19:33:49 +0600

    added all the logics & interactive all the pages

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,8 @@
 //     playSection.classList.remove('hidden');
 // }
 
+
+
 function continueGame() {
     const alphabet = getARandomAlphabet();
     // console.log('Your random alphabet is:', alphabet);
@@ -21,6 +23,75 @@ function continueGame() {
 
 function play() {
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
+
     continueGame();
 }
+
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    const gameScore = getTextElementValueById('current-score');
+    setTextElementValueById('game-score', gameScore);
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+}
+
+// handle key press
+function handleKeyBoardBtnPress(event) {
+    const playerPressed = event.key;
+    // console.log('Player pressed: ', playerPressed);
+    if (playerPressed === 'Escape') {
+        gameOver();
+    }
+
+    // get expected key press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText.toLowerCase();
+
+    console.log(playerPressed, currentAlphabet);
+
+    // check matched or not
+    if (playerPressed === currentAlphabet) {
+        // update score
+        // 1.get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        // 2.Increase the score by 
+        const newScore = currentScore + 1;
+
+        // 3.Display the score;
+        currentScoreElement.innerText = newScore;
+
+        removeBackgroundColorById(currentAlphabet);
+        continueGame();
+    }
+    else {
+
+        // 1.get the current life 
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        // 2.decrease the life count by 1
+        const newLife = currentLife - 1;
+        // 3. display the updated life count
+        currentLifeElement.innerText = newLife;
+
+
+        if (newLife === 0) {
+            gameOver();
+        }
+    }
+}
+
+// capture keyboard keypress
+document.addEventListener('keyup', handleKeyBoardBtnPress);
